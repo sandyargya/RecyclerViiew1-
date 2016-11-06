38acdeb099c2f434ae9d665b0f7adc20d05dde82
sandy <sandyargya123@gmail.com>
2016-11-06 10:11:56 +0700

    RecyclerView List

---
+++ app/src/main/java/model/Hotel.java
@@ -0,0 +1,19 @@
+package model;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by sandy on 06/11/2016.
+ */
+
+public class Hotel {
+    public String judul;
+    public String deskripsi;
+    public Drawable foto;
+
+    public Hotel(String judul, String deskripsi, Drawable foto) {
+        this.judul = judul;
+        this.deskripsi = deskripsi;
+        this.foto = foto;
+    }
+}
